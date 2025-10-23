58076d846b4b2f18ced3aa552b644f2d5a0ed0df
Mung Ho <mung.hokha@gmail.com>
2025-10-23 20:33:29 +0700

    Clean up old folder and fix case-sensitive issue

--- blog_RESTful/src/main/java/com/example/blog/service/IBlogTypeService.java
+++
@@ -1,9 +0,0 @@
-package com.example.blog.service;
-
-import com.example.blog.entity.BlogType;
-
-import java.util.List;
-
-public interface IBlogTypeService {
-    List<BlogType> findAll();
-}
--- blog_restful/src/main/java/com/example/blog/repository/IBlogTypeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.blog.repository;
-
-import com.example.blog.entity.BlogType;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IBlogTypeRepository extends JpaRepository<BlogType, Integer>{
-}
